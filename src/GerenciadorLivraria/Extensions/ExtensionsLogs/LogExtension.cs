39f6d171832d9dbb3b5aeb77ed763db9f8afcd76
pedro <pedro@PEDROIGHORNOTEB>
2026-05-11 10:28:18 -0300

    feat: adiciona configuração de logs com Serilog

---
+++ src/GerenciadorLivraria/Extensions/ExtensionsLogs/LogExtension.cs
@@ -0,0 +1,38 @@
+﻿using Serilog;
+
+namespace GerenciadorLivraria.Extensions.ExtensionsLogs
+{
+    public class LogExtension
+    {
+        private static readonly string LogDirectory = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            "Downloads",
+            "Gerenciador-Livraria"
+        );
+
+        static LogExtension()
+        {
+            InitializeLogger();
+        }
+
+        public static void InitializeLogger()
+        {
+            try
+            {
+                Directory.CreateDirectory(LogDirectory);
+                Log.Logger = new LoggerConfiguration()
+                    .WriteTo.File(Path.Combine(LogDirectory, "log-.txt"), rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true, fileSizeLimitBytes: 1000000)
+                    .CreateLogger();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to start the logger: {ex.Message}");
+            }
+        }
+
+        public static ILogger GetLogger()
+        {
+            return Log.Logger;
+        }
+    }
+}
