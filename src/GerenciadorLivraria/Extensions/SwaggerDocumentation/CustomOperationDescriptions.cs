bc481474a1765aee744cb11bde5b5ae931060180
pedro <pedro@PEDROIGHORNOTEB>
2026-05-11 10:28:50 -0300

    feat: adiciona customização de documentação Swagger

---
+++ src/GerenciadorLivraria/Extensions/SwaggerDocumentation/CustomOperationDescriptions.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace GerenciadorLivraria.Extensions.SwaggerDocumentation
+{
+    public class CustomOperationDescriptions : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (context?.ApiDescription?.HttpMethod is null || context.ApiDescription.RelativePath is null)
+                return;
+
+            var path = context.ApiDescription.RelativePath.ToLowerInvariant();
+
+            var routeHandlers = new Dictionary<string, Action>(StringComparer.OrdinalIgnoreCase)
+            {
+            };
+
+            foreach (var kv in routeHandlers
+                     .OrderByDescending(k => k.Key.Length))
+            {
+                if (path.Contains(kv.Key))
+                {
+                    kv.Value.Invoke();
+                    return;
+                }
+            }
+        }
+
+        private void HandleUsersOperations(OpenApiOperation operation, OperationFilterContext context)
+        {
+            
+        }
+
+        private void HandleGamesOperations(OpenApiOperation operation, OperationFilterContext context)
+        {
+            
+        }
+
+        private void AddResponses(OpenApiOperation operation, string statusCode, string description)
+        {
+            if (!operation.Responses.ContainsKey(statusCode))
+            {
+                operation.Responses.Add(statusCode, new OpenApiResponse { Description = description });
+            }
+        }
+    }
+}
