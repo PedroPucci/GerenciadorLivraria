c288d8b4283dcf5b6b96bb27963540b9541b994b
pedro <pedro@PEDROIGHORNOTEB>
2026-05-17 10:55:34 -0300

    feat: adiciona entidade BookEntity e enum BookGenre

---
+++ src/GerenciadorLivraria.Domain/Entities/BookEntity.cs
@@ -0,0 +1,20 @@
+﻿using GerenciadorLivraria.Domain.Enums;
+
+namespace GerenciadorLivraria.Domain.Entities
+{
+    public class BookEntity
+    {
+        public Guid Id { get; set; }
+        public string? Title { get; set; }
+        public string? Author { get; set; }        
+        public BookGenre Genre { get; set; }
+        public string? ISBN { get; set; }
+        public string? Publisher { get; set; }
+        public int PublishedYear { get; set; }
+        public string? Description { get; set; }
+
+        public bool IsActive { get; set; }
+        public DateTime? CreateDate { get; set; } = DateTime.UtcNow;
+        public DateTime? ModificationDate { get; set; }
+    }
+}
---
+++ src/GerenciadorLivraria.Domain/Enums/BookGenre.cs
@@ -0,0 +1,16 @@
+﻿namespace GerenciadorLivraria.Domain.Enums
+{
+    public enum BookGenre
+    {
+        Fiction = 1,
+        Romance = 2,
+        Mystery = 3,
+        Fantasy = 4,
+        ScienceFiction = 5,
+        Horror = 6,
+        Biography = 7,
+        Drama = 8,
+        Adventure = 9,
+        Thriller = 10
+    }
+}
