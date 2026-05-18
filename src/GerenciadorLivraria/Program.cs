69de982bfd7b26e52c69511597877cfe42626637
pedro <pedro@PEDROIGHORNOTEB>
2026-05-18 08:21:20 -0300

    feat: adiciona endpoint de health check customizado

--- src/GerenciadorLivraria/Program.cs
+++ src/GerenciadorLivraria/Program.cs
@@ -1,7 +1,9 @@
 using GerenciadorLivraria.Extensions;
 using GerenciadorLivraria.Extensions.ExtensionsLogs;
 using GerenciadorLivraria.Infrastructure.Connections;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +11,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddSession();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddApplicationServices(builder.Configuration);
-builder.Services.AddHealthChecks().AddDbContextCheck<DataContext>();
+//builder.Services.AddHealthChecks().AddDbContextCheck<DataContext>();
+builder.Services.AddHealthChecks().AddDbContextCheck<DataContext>("sqlserver");
 
 LogExtension.InitializeLogger();
 var loggerSerialLog = LogExtension.GetLogger();
@@ -43,7 +46,27 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-app.MapHealthChecks("/health");
+//app.MapHealthChecks("/health"); //padrao
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = async (context, report) =>
+    {
+        var result = new
+        {
+            status = report.Status.ToString(),
+            checks = report.Entries.Select(x => new
+            {
+                name = x.Key,
+                status = x.Value.Status.ToString()
+            })
+        };
+
+        context.Response.ContentType = "application/json";
+
+        await context.Response.WriteAsync(JsonSerializer.Serialize(result));
+    }
+});
 
 var runMigrations = builder.Configuration.GetValue<bool>("RunMigrations");
 if (runMigrations)
