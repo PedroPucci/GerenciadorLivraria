dda5d022dc7d4a5811974832b10b4486e079b9ce
pedro <pedro@PEDROIGHORNOTEB>
2026-05-18 14:41:40 -0300

    refactor: remover validações duplicadas de senha do FluentValidation

--- src/GerenciadorLivraria.Application/Validators/UserRequestValidator.cs
+++ src/GerenciadorLivraria.Application/Validators/UserRequestValidator.cs
@@ -9,20 +9,6 @@ namespace GerenciadorLivraria.Application.Validators
     {
         public UserRequestValidator()
         {
-            RuleFor(p => p.Email)
-                .NotEmpty()
-                    .WithMessage(UserErrors.User_Error_EmailCanNotBeNullOrEmpty.Description())
-                .Matches(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
-                    .WithMessage(UserErrors.User_Error_InvalidEmailFormat.Description());
-
-            RuleFor(p => p.Password)
-                .NotEmpty()
-                    .WithMessage(UserErrors.User_Error_PasswordCanNotBeNullOrEmpty.Description())
-                .MinimumLength(8)
-                    .WithMessage(UserErrors.User_Error_PasswordLengthLessEight.Description())
-                .Matches(@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[^A-Za-z0-9]).{8,}$")
-                    .WithMessage(UserErrors.User_Error_PasswordInvalid.Description());
-
             RuleFor(p => p.Name)
                 .NotEmpty()
                     .WithMessage(UserErrors.User_Error_NameCanNotBeNullOrEmpty.Description())
